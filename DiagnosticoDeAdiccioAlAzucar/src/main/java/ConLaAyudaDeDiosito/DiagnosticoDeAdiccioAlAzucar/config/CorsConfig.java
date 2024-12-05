cfa2855ddbd6b0d50644bac18f31d7e9052cce19
Juan Cardona <jcardonabageth@gmail.com>
2024-12-05 15:01:20 -0500

    fix: cors config origin error 2

--- DiagnosticoDeAdiccioAlAzucar/src/main/java/ConLaAyudaDeDiosito/DiagnosticoDeAdiccioAlAzucar/config/CorsConfig.java
+++ DiagnosticoDeAdiccioAlAzucar/src/main/java/ConLaAyudaDeDiosito/DiagnosticoDeAdiccioAlAzucar/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package ConLaAyudaDeDiosito.DiagnosticoDeAdiccioAlAzucar.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Autowired
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*") // Cambia * por la URL permitida si es específica
                 . allowedMethods("GET", "POST", "PUT", "DELETE", "PATH") // Métodos HTTP permitidos
-                .allowedHeaders("*") // Headers permitidos
-                .allowCredentials(true); // Permite enviar cookies o credenciales si es necesario
+                .allowedHeaders("*"); // Headers permitidos
     }
 }
