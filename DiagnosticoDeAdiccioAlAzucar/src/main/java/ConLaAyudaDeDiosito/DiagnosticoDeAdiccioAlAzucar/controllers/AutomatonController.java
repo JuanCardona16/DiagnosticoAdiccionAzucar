1090b3dc174d72ff95277a9c00a63c6e70297469
Juan Cardona <jcardonabageth@gmail.com>
2024-12-05 14:55:12 -0500

    fix: cors config origin error

--- DiagnosticoDeAdiccioAlAzucar/src/main/java/ConLaAyudaDeDiosito/DiagnosticoDeAdiccioAlAzucar/controllers/AutomatonController.java
+++ DiagnosticoDeAdiccioAlAzucar/src/main/java/ConLaAyudaDeDiosito/DiagnosticoDeAdiccioAlAzucar/controllers/AutomatonController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/automaton")
-@CrossOrigin(origins = "*")
 public class AutomatonController {
 
     @Autowired
