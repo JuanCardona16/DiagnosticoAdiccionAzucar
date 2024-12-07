f2e96064bad552d6c87d387b1aa000f143edd35e
Juan Cardona <jcardonabageth@gmail.com>
2024-12-06 23:05:57 -0500

    fix: change api url

--- DianosticoDeAdiccionAlAzucarCliente/src/config/api/ApiConfig.ts
+++ DianosticoDeAdiccionAlAzucarCliente/src/config/api/ApiConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiUrl = axios.create({
-  baseURL: "http://localhost:8080/api/v1/automaton",
-  // baseURL: "https://diagnosticoadiccionazucar.onrender.com/api/v1/automaton",
+  // baseURL: "http://localhost:8080/api/v1/automaton",
+  baseURL: "https://diagnosticoadiccionazucar.onrender.com/api/v1/automaton",
 });
